1b23f4826a508111602e91f9fc7dca9cedd7af8f
Ricardokim94 <cmrikim@naver.com>
2024-06-14 00:43:53 +0900

    data보여주고, 버튼누르면 dataChange, 그옆 버튼 누르면 원래대로 구현

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,30 +1,68 @@
-import React from 'react'
-import { Address, Restaurant } from './model/restuarnt'
+import React from 'react';
+import { Address, Restaurant } from './model/restuarnt';
 
-//여기에서만 쓰이는 타입하나를 만듬 (props 사용하기 위해서)
-interface OwnProps{
-  info : Restaurant,
-  changeAddress(address:Address) : void;    //함수 타입 지정은 이렇게 한다. 
-                                  //void : return 타입이 x때 사용한다.
+interface OwnProps {
+  info: Restaurant;
+  changeAddress(address: Address): void;
 }
 
-const Store : React.FC<OwnProps> = ({info, changeAddress}) => { //React.FC에서도 <> 쓸수 있다.
-  //change 하는 함수 구현해봄
-  const handleButtonClick = ()=> {
-    const newAddress : Address = {
-      city : 'new City',
-      detail : 'new Detail',
-      zipCode : 456456
+const Store: React.FC<OwnProps> = ({ info, changeAddress }) => {
+  const handleButtonClick = () => {
+    const newAddress: Address = {
+      city: 'new city',
+      detail: 'new detail',
+      zipCode: 456456
     };
     changeAddress(newAddress);
   };
-  
+
+  const handleExistingButtonClick = () => {
+    // 기존의 주소 정보로 되돌리기
+    const originalAddress: Address = {
+      city: 'suwon',
+      detail: 'somewhere',
+      zipCode: 123123
+    };
+    changeAddress(originalAddress);
+  };
+
   return (
-    <div>
-      <div>{info.name}</div>
-      <button onClick={handleButtonClick}>Change Address</button>
+    <div className="container mt-4">
+      <table className="table table-bordered">
+        <thead className="thead-light">
+          <tr>
+            <th>Name</th>
+            <th>City</th>
+            <th>Detail</th>
+            <th>Zip Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{info.name}</td>
+            <td>{info.address.city}</td>
+            <td>{info.address.detail}</td>
+            <td>{info.address.zipCode}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div style={{ marginTop: '10px' }}> {/* 버튼 사이에 간격을 주기 위한 div */}
+        <button 
+          onClick={handleButtonClick} 
+          className="btn btn-primary"
+          style={{ marginRight: '10px' }} // 오른쪽 마진 추가
+        >
+          Change Address
+        </button>
+        <button 
+          onClick={handleExistingButtonClick} 
+          className="btn btn-primary"
+        >
+          Existing Button
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Store;
